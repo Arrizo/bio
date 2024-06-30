2a61a44a06ae86ab762ea3080147a57163e0ce37
Arrizo <arrizo787392502@163.com>
2024-06-30 21:59:49 +0800

    m

--- src/api/name.ts
+++ src/api/name.ts
@@ -1,9 +1,12 @@
 /*
  * @Author: chenzechao
  * @Date: 2024-06-29 20:11:14
- * @LastEditTime: 2024-06-29 20:11:25
+ * @LastEditTime: 2024-06-30 21:47:18
  * @LastEditors: chenzechao
  * @Description:
- * @FilePath: /tius/bio/src/api/name.ts
+ * @FilePath: /bio/src/api/name.ts
  */
 export const dfad = () => {};
+export const getNAme = (a: string = '') => {
+  return a.toString();
+};
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,10 +1,10 @@
 <!--
  * @Author: chenzechao
  * @Date: 2024-06-29 16:12:31
- * @LastEditTime: 2024-06-29 20:15:56
+ * @LastEditTime: 2024-06-30 21:48:07
  * @LastEditors: chenzechao
  * @Description: 
- * @FilePath: /tius/bio/src/views/home/index.vue
+ * @FilePath: /bio/src/views/home/index.vue
 -->
 <template>
   <div class="asdfa">
@@ -15,13 +15,15 @@
   </div>
 </template>
 <script setup lang="ts" name="home">
-  import {} from '@/api/';
+  import { dfad, getNAme } from '@/api/name';
   import { reactive, ref } from 'vue';
   const state = reactive({
     name: '2323',
     key: 'asdfasdfadad'
   });
+  console.log(getNAme());
   const gg = '';
+  dfad();
   gg = 10;
   const tabName = ref<any>('');
 
